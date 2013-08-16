bb44132bdc3e5ae320ded7093f86ac780a86632e
Dominik Wagner <dominik-wagner@live.de>
2013-08-16 21:02:56 +0200

    init

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dominik Wagner
+ * Date: 16.08.13
+ * Time: 21:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main (String [] args){
+        System.out.println("Dominik");
+    }
+}
